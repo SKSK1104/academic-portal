bf704f87c11b7460364dee5e83f1c0233627ee82
SKSK1104 <sekolah-8311-cm1@moe-dl.edu.my>
2026-09-10 11:03:39 +0800

    Load parent report styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import './critique-fixes.css';
 import './clean-copy.css';
 import './hotfix.css';
 import './pbd-analysis-fix.css';
+import './parent-report.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
